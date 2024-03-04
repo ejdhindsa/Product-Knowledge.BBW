0d419a35b9e8cc3d48019fffd44606cbf2222681
ejdhindsa <ejdhindsa@Gmail.com>
2024-03-04 14:40:55 -0600

    New Interface

---
+++ Product.java
@@ -0,0 +1,4 @@
+public interface Product
+{
+    
+} // end of Product
--- Shop.java
+++
@@ -1,8 +0,0 @@
-// import statements
-import java.util.ArrayList;
-
-public abstract class Shop
-{
-    protected ArrayList<Product> products;
-    
-}
